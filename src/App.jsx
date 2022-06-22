fa53eb9278e3605b5837d380e4f890b460508597
nvanha <nvha1120@gmail.com>
2022-06-22 09:49:40 +0700

    Adding Cognito Signup and Login with AWS Amplify

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+
+Amplify.configure(awsconfig);
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AmplifySignOut />
+        <h2>DEV - My App Content</h2>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
