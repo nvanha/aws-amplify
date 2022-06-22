7300244806f85af003ccfab68112ff22bc5778ce
nvanha <nvha1120@gmail.com>
2022-06-22 10:31:02 +0700

    Adding Cognito Signup and Login with AWS Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsamplify4609ce0c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "songAPI": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import Amplify from "aws-amplify";
-import awsconfig from "./aws-exports";
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import { useEffect, useState } from "react";
+import awsconfig from "./aws-exports";
+import { listSongs } from "./graphql/queries";
 
 Amplify.configure(awsconfig);
 
-function App() {
+const App = () => {
+  const [songs, setSongs] = useState([]);
+  console.log("songs: ", songs);
+
+  const fetchSong = async () => {
+    try {
+      const songData = await API.graphql(graphqlOperation(listSongs));
+      const songList = songData.data.listSongs.items;
+      console.log("song list", songList);
+      setSongs(songList);
+    } catch (error) {
+      console.error("error on fetching songs: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchSong();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +33,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default withAuthenticator(App);
